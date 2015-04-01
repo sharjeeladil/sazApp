7ba22004ab7b51d15e69d07d9f14ab87f55e9ad3
Sharjeel Adil Zadah <sharjeeladil@gmail.com>
2015-04-01 19:31:30 -0400

    Image Addition

--- www/js/view.js
+++ www/js/view.js
@@ -13,53 +13,71 @@ var app = function(app) {
 		p.main = new createjs.Container();		
 		p.main.name = "main";	
 		p.main.setBounds(0,0,stageW,stageH);
-		
-		var logo = new zim.Rectangle(100, 100, "orange");	
-		logo.setBounds(0,0,100,100);
-		p.main.addChild(logo);
-		
+
 		var content = p.main.content = new createjs.Container();
 		content.setBounds(0,0,600,600);
-		var thing = p.mainThing = new zim.Rectangle(200,200,"red");
-		thing.x = 100;
-		thing.y = 100;		
-		content.addChild(thing);
-		p.main.addChild(content);
 		
-		var related = new zim.Rectangle(200, 500, "pink");	
-		related.setBounds(0,0,200,500);
-		p.main.addChild(related);
+		var textContainer = p.main.textContainer = new createjs.Container();
+		textContainer.x = 100;
+		textContainer.y = 100;
+		p.main.addChild(textContainer);
 		
+		var  labelText = "Don't Swap or he will be Angry";
+		var introText = new zim.Label(labelText, 40, "arial", "green");
+		console.log (labelText);
+		textContainer.addChild(introText);
 		
-		gridManager.add( new zim.Grid(content) );
-		guideManager.add( new zim.Guide(content) );
-		guideManager.add( new zim.Guide(content, false) );
-		
-		zog(layoutManager);
-		
+		var human = p.main.human = new createjs.Container();
+        p.main.addChild(human);
+        human.scaleX =1;
+        human.scaleY = 1;
+
+        var humanImg = new createjs.Bitmap("images/human.png");
+        human.x = frame.stageW/2;
+        human.y = frame.stageH/6;
+        human.addChild(humanImg);
+		p.main.addChild(human);
+	
 		var mainParts = [
-{object:logo, marginLeft:5, maxHeight:100, width:20, valign:"top"},
-{object:content, marginLeft:5, maxHeight:100, valign:"top", align:"middle", backgroundColor:"beige"}, 
-{object:related, marginLeft:5, maxHeight:100, minWidth:20, align:"left", valign:"top"}
+	//{object:logo, marginLeft:5, maxHeight:100, width:20, valign:"top"},
+	{object:content, marginLeft:5, maxHeight:100, valign:"top", align:"middle", backgroundColor:"black"}, 
+	//{object:related, marginLeft:5, maxHeight:100, minWidth:20, align:"left", valign:"top"}
 			];
 		
 		var mainLayout = new zim.Layout(p.main, mainParts, 5, "black", false, new createjs.Shape(), stage);
 		
 		layoutManager.add(mainLayout);	
 		
-		
-		
-		
 		p.info = new createjs.Container();		
 		p.info.name = "info";		
-		var infoBacking = new zim.Rectangle(stageW, stageH, "yellow");	
+		var infoBacking = new zim.Rectangle(stageW, stageH, "green");	
 		infoBacking.setBounds(0,0,stageW,stageH);
 		p.info.addChild(infoBacking);
-				
+		
+		var hulk = p.info.hulk = new createjs.Container();
+        p.info.addChild(hulk);
+        hulk.scaleX =1;
+        hulk.scaleY = 1;
+
+        var hulkImg = new createjs.Bitmap("images/hulk.png");
+        hulk.x = frame.stageW/2;
+        hulk.y = frame.stageH/6;
+        hulk.addChild(hulkImg);
+		p.info.addChild(hulk);
+		
+		var textContainer = p.info.textContainer = new createjs.Container();
+		textContainer.x = 50;
+		textContainer.y = 50;
+		p.info.addChild(textContainer);
+		
+		var  labelText = "I will find you and Kill you!";
+		var introText = new zim.Label(labelText, 25, "arial", "orange");
+		console.log (labelText);
+		textContainer.addChild(introText);
+		
 		return p;
 		
 	}
-	
 	app.makeVerticalPages = function(layoutManager, gridManager, guideManager) {
 		
 		zog("pages");
@@ -70,43 +88,65 @@ var app = function(app) {
 		p.main.name = "main";	
 		p.main.setBounds(0,0,stageW,stageH);
 		
-		var logo = new zim.Rectangle(100, 100, "orange");	
-		logo.setBounds(0,0,100,100);
-		p.main.addChild(logo);
+		var human = p.main.human = new createjs.Container();
+        p.main.addChild(human);
+        human.scaleX =1;
+        human.scaleY = 1;
+
+        var humanImg = new createjs.Bitmap("images/human.png");
+        human.x = frame.stageW/2;
+        human.y = frame.stageH/6;
+        human.addChild(humanImg);
+		p.main.addChild(human);
 		
+		var textContainer = p.main.textContainer = new createjs.Container();
+		textContainer.x = 50;
+		textContainer.y = 50;
+		p.main.addChild(textContainer);
+		
+		var  labelText = "Don't Swap or he will be Angry";
+		var introText = new zim.Label(labelText, 16, "arial", "green");
+		console.log (labelText);
+		textContainer.addChild(introText);
+			
 		var content = p.main.content = new createjs.Container();
 		content.setBounds(0,0,600,600);
-		var thing = p.mainThing = new zim.Rectangle(200,200,"red");
-		thing.x = 100;
-		thing.y = 100;		
-		content.addChild(thing);
-		p.main.addChild(content);
-		
-		var related = new zim.Rectangle(500, 200, "pink");	
-		related.setBounds(0,0,500,200);
-		p.main.addChild(related);
-		
-		gridManager.add( new zim.Grid(content) );
-		guideManager.add( new zim.Guide(content) );
-		guideManager.add( new zim.Guide(content, false) );
-		
-		zog(layoutManager);
-		
+				
 		var mainParts = [ 
-	{object:logo, marginTop:10, maxWidth:80, minHeight:20, align:"left", valign:"top"},
-	{object:content, marginTop:5, maxWidth:80, backgroundColor:"beige"}, 
-	{object:related, marginTop:5, maxWidth:80, height:20, backgroundColor:"red"} ];
+	//{object:logo, marginTop:10, maxWidth:80, minHeight:20, align:"left", valign:"top"},
+	{object:content, marginTop:5, maxWidth:95, height:90, backgroundColor:"black"},
+	//{object:related, marginTop:5, maxWidth:95, height:90, backgroundColor:"black"}
+	 ];
 		
-		var mainLayout = new zim.Layout(p.main, mainParts, 5, "black", true, new createjs.Shape(), stage);
-		
+		var mainLayout = new zim.Layout(p.main, mainParts, 5, "green", true, new createjs.Shape(), stage);
 		layoutManager.add(mainLayout);	
-		
-		
+			
 		p.info = new createjs.Container();		
 		p.info.name = "info";		
-		var infoBacking = new zim.Rectangle(stageW, stageH, "yellow");	
+		var infoBacking = new zim.Rectangle(stageW, stageH, "green");	
 		infoBacking.setBounds(0,0,stageW,stageH);
 		p.info.addChild(infoBacking);
+			
+		var hulk = p.info.hulk = new createjs.Container();
+        p.info.addChild(hulk);
+        hulk.scaleX =1;
+        hulk.scaleY = 1;
+
+        var hulkImg = new createjs.Bitmap("images/hulk.png");
+        hulk.x = frame.stageW/2;
+        hulk.y = frame.stageH/6;
+        hulk.addChild(hulkImg);
+		p.info.addChild(hulk);
+		
+		var textContainer = p.info.textContainer = new createjs.Container();
+		textContainer.x = 50;
+		textContainer.y = 50;
+		p.info.addChild(textContainer);
+		
+		var  labelText = "I will find you and Kill you!";
+		var introText = new zim.Label(labelText, 25, "arial", "orange");
+		console.log (labelText);
+		textContainer.addChild(introText);
 				
 		return p;
 		
